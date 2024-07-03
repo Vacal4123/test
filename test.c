7b1c59a3dfa21e9d07bac0a91734891a2475766c
Kim Jinwoo <vacal412@naver.com>
2024-07-03 15:41:44 +0900

    test.c 수정

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void multiplication_table() {
+	for (int i = 1; i < 10; i++) {
+		for (int j = 2; j < 10; j++)
+			printf("%d X %d = %2d  ", j, i, i * j);
+		printf("\n");
+	}
+}
+
+int main() {
+	multiplication_table();
+	return 0;
+}
