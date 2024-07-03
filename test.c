25f0a9d84e2ed7083503f89b0832b26d32c45199
Kim Jinwoo <vacal412@naver.com>
2024-07-03 15:51:54 +0900

    minus_table() 수정

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 void minus_table() {
 	for (int i = 1; i < 10; i++) {
 		for (int j = 2; j < 10; j++)
-			printf("%d - %d = %2d  ", j, i, i - j);
+			printf("%d - %d = %2d  ", j, i, j - i);
 		printf("\n");
 	}
 }
