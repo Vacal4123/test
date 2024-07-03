68540c3238039a81582716894d732c9d16154ec8
Kim Jinwoo <vacal412@naver.com>
2024-07-03 15:44:40 +0900

    test.c에 minus_table() 추가

--- test.c
+++ test.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void minus_table() {
+	for (int i = 1; i < 10; i++) {
+		for (int j = 2; j < 10; j++)
+			printf("%d - %d = %2d  ", j, i, i - j);
+		printf("\n");
+	}
+}
+
 void multiplication_table() {
 	for (int i = 1; i < 10; i++) {
 		for (int j = 2; j < 10; j++)
@@ -10,5 +18,6 @@ void multiplication_table() {
 
 int main() {
 	multiplication_table();
+	minus_table();
 	return 0;
 }
